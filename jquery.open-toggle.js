58f9deec16ddac7cc55dc5ae9b21bf3ee3d41155
neel.dedhia <neel.dedhia@sakec.ac.in>
2019-10-04 00:30:26 +0530

    Feat: toggle class as well as an associated element in href attribute

--- jquery.open-toggle.js
+++ jquery.open-toggle.js
@@ -9,27 +9,45 @@
  */
 (function($) {
   $.fn.openToggle = function(className = "open") {
-    var wasClicked = false,
-      that = this;
+    var wasClicked = false;
+    var clickedBlockId = null;
+    that = this;
+    $(that).each(function(index, curEle){
+      if($(curEle).attr('href') != undefined) $($(curEle).attr('href')).show().toggle();
+    });
 
     that.click(function(e) {
+      e.preventDefault();
       wasClicked = true;
       var ele = $(this);
-      that.not(ele).removeClass(className);
+      clickedBlockId = ele.attr('href');
+
+      if(that.not(ele).length > 0){
+         $(that.not(ele)).each(function(index, otherThanCurrent){
+            $(otherThanCurrent).removeClass(className);
+            if($(otherThanCurrent).attr('href') != undefined) $($(otherThanCurrent).attr('href')).hide();
+         }); 
+      }
 
       if (ele.is("." + className + ".no-self-toggle")) {
         return;
       }
 
       ele.toggleClass(className);
+      if($(ele).attr('href') != undefined) $($(ele).attr('href')).toggle();
+
     });
+
+    $count = 0 ;
 
     $(document).click(function(e) {
       e.stopPropagation();
-      if (!wasClicked) {
+      isntClickedBlockId = (e.target.hasAttribute("id"))? ((('#'+e.target.attributes.id.value) !== clickedBlockId)): true;
+      if (!wasClicked && isntClickedBlockId) {
         that.removeClass(className);
+        if(clickedBlockId != null) $(clickedBlockId).hide();
       }
-      wasClicked = false;
+      wasClicked = clickedBlockId = false;
     });
   };
 })(window.jQuery || window.Zepto);
